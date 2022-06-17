cdf81f2332863b239231de7c468e8312298e8344
Dimitrios J. Vergados <djvergad@gmail.com>
2022-06-17 06:58:27 +0300

    Fix central db url

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -10,7 +10,9 @@ https://connexion.readthedocs.io/en/latest/security.html
 
 def check_oAuth2(token):
     if not os.environ.get("SAMPLE_DATA"):
-        auth = requests.get('http://localhost:5000/authorization/', params={
+        db_url = os.getenv(
+            "CENTRAL_DB_BASE_URL") or 'https://db.flexgrid-project.eu'
+        auth = requests.get(f'{db_url}/authorization/', params={
             'token': token,
             'resource': 'atp',
             'method': 'post',
